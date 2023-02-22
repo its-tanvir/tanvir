a41374f00f87aacf7184f1ba1fd806d4e2db83b6
its-tanvir <125755561+its-tanvir@users.noreply.github.com>
2023-02-22 06:12:52 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation  ============================= */
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","web Developer","Graphic Designer"],
+    typeSpeed:100,
+    BackSpeed:40,
+    loop:true
+})
